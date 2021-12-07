627dea52903f48bad660b3f1cdb22372d7d59673
James Sibrel III <jamessibreliii@gmail.com>
2021-12-06 22:24:32 -0500

    Removed useless code added notes

--- Linear Regression/Stock Guesses.py
+++ Linear Regression/Stock Guesses.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 import pandas.util
 import os
 import datetime
-#os.path.dirname(__file__)
-SampleData=pd.read_csv('C:\\Users\\james\\Documents\\Python\\Machine Learning Fun\\Linear Regression' + '\\' + 'ExampleStockData.csv')
+
+SampleData=pd.read_csv(os.path.dirname(__file__) + '\\' + 'ExampleStockData.csv')
 print(SampleData.head())
 
 Ticker = ""
 
-
+#wait for user to pick an accurate ticker
 while Ticker == "":
     print("Please enter a stock name from the following list:")
     i = 0
@@ -30,36 +30,43 @@ while Ticker == "":
     else:
         print("Please enter a value on the list")
 filteredData = SampleData[SampleData['ticker'] == Ticker]
-def formatasDate(x):
-    splitdate = x.split('-')
-    return datetime.date(int(splitdate[0]),int(splitdate[1]),int(splitdate[2]))
 filteredData['date'] = pd.to_datetime(filteredData['date'])
 filteredData['date']=filteredData['date'].apply(lambda x:  x.toordinal())
 filteredData = filteredData.sort_values(by=['date'],ascending= True)
 print(filteredData.head())
 
-#TO Do
-#Add code to do simple linear regression 
-LRequation = lambda x,m,b : x * m + b
-## X*m = Y
-## X^T * X * B = X^T *Y  
-## B = [X^T * X]^-1 * Y
+#simple linear equation
+# m = slope, c = y intercept 
+##### x*m + c = y
+#apply matrix variables to the equation X = [[series of dates],[series of 1s]], B = [[slope(B0)],[intercept(B1)]],Y = [[series of opening prices]] 
+##### X * B = Y
+#apply the X transpose to the left of both sides
+##### (X^T) * X * B = (X^T) *Y  
+#apply the inverse of the left side to both sides 
+##### B = [(X^T) * X]^-1 (X^T) * Y
 
+#get cols as numpy arrays
 datecol = filteredData['date'].to_numpy(np.matrix).astype(np.double)
 opencol = filteredData['open'].to_numpy(np.matrix).astype(np.double)
 
+#Create X matrix 
+#first col is the date values 
+#second col is filled with 1s for the y intercept
 X = np.c_[datecol,np.ones([datecol.size,1])]
 Y = np.c_[opencol]
 XT = X.transpose()
 
+## B = [(X^T) * X]^-1 (X^T) * Y
 B = (np.matmul(np.linalg.inv(np.matmul(XT, X)) ,np.matmul(XT , Y)))
 
+#Generate Equation Values
 Xline = [] 
 Yline = []
 for i in range(datecol.size):
     Xline.append(datecol[i])
     Yline.append(B[0][0]*datecol[i] + B[1][0])
 
+#Plot Everything
 plt.plot_date(datecol,opencol)
 plt.plot(Xline,Yline)
 plt.show()
