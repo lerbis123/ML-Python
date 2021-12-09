87ecd2b615c515d916a1c073245f3bf87814fc20
James Sibrel III <jamessibreliii@gmail.com>
2021-12-08 19:31:41 -0500

    Guessing Capability
    Added Guessing Capability
    Cleaned Up Code
    Added  an epoch column, GetLineOfBestFit function and a normalization function

--- Linear Regression/Stock Guesses.py
+++ Linear Regression/Stock Guesses.py
@@ -2,6 +2,7 @@ from numpy.core.fromnumeric import transpose
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from pandas._libs.tslibs.timestamps import Timestamp
 import pandas.util
 import os
 import datetime
@@ -31,9 +32,22 @@ while Ticker == "":
         print("Please enter a value on the list")
 filteredData = SampleData[SampleData['ticker'] == Ticker]
 filteredData['date'] = pd.to_datetime(filteredData['date'])
-filteredData['date']=filteredData['date'].apply(lambda x:  x.toordinal())
-filteredData = filteredData.sort_values(by=['date'],ascending= True)
+def applyepoch(x):
+    ts = Timestamp(x)
+    return datetime.datetime(year=ts.year,month=ts.month, day=ts.day).timestamp()
+epoch = filteredData['date'].apply(applyepoch)
+filteredData["epoch"] = epoch
+filteredData = filteredData.sort_values(by=['epoch'],ascending= True)
 print(filteredData.head())
+
+#normalize epoch col
+def Normalize(data: np.array):
+    temp1 = np.array(data)
+    MyMin,MyMax =  temp1.min(),temp1.max()
+    retlist = np.array([])
+    for d in range(temp1.size):
+       retlist = np.append(retlist, (temp1[d] - MyMin)/(MyMax-MyMin))
+    return retlist,MyMax,MyMin
 
 #simple linear equation
 # m = slope, c = y intercept 
@@ -46,27 +60,47 @@ print(filteredData.head())
 ##### B = [(X^T) * X]^-1 (X^T) * Y
 
 #get cols as numpy arrays
-datecol = filteredData['date'].to_numpy(np.matrix).astype(np.double)
+NormAndInfo= Normalize(filteredData['epoch'].to_numpy(np.matrix).astype(np.double))
+NormEpochCol = NormAndInfo[0]
 opencol = filteredData['open'].to_numpy(np.matrix).astype(np.double)
+def GetLineOfBestFit(Xcol,Ycol):
+    #Create X matrix 
+    #first col is the date values 
+    #second col is filled with 1s for the y intercept
+    X = np.c_[Xcol,np.ones([Xcol.size,1])]
+    Y = np.c_[Ycol]
+    XT = X.transpose()
 
-#Create X matrix 
-#first col is the date values 
-#second col is filled with 1s for the y intercept
-X = np.c_[datecol,np.ones([datecol.size,1])]
-Y = np.c_[opencol]
-XT = X.transpose()
+    ## B = [(X^T) * X]^-1 (X^T) * Y
+    B = (np.matmul(np.linalg.inv(np.matmul(XT, X)) ,np.matmul(XT , Y)))
+    return lambda x: B[0][0]*x + B[1][0]
 
-## B = [(X^T) * X]^-1 (X^T) * Y
-B = (np.matmul(np.linalg.inv(np.matmul(XT, X)) ,np.matmul(XT , Y)))
+#Our Line Of Best Fit
+LOBF= GetLineOfBestFit(NormEpochCol,opencol)
 
 #Generate Equation Values
 Xline = [] 
 Yline = []
-for i in range(datecol.size):
-    Xline.append(datecol[i])
-    Yline.append(B[0][0]*datecol[i] + B[1][0])
+for i in range(NormEpochCol.size):
+    Xline.append(filteredData["date"].to_numpy()[i])
+    Yline.append(LOBF(NormEpochCol[i]))
+
 
 #Plot Everything
-plt.plot_date(datecol,opencol)
+plt.plot(filteredData["date"].to_numpy(),opencol)
 plt.plot(Xline,Yline)
 plt.show()
+
+#Add Stock Date Guessing
+ret = input("Please Enter A Date To Guess the stock price at (Numeric Format: Year/Month/day)")
+
+daymonthyear = ret.split("/")
+
+NormalizationMax = NormAndInfo[1]
+NormalizationMin = NormAndInfo[2]
+
+def getSingleEpoch(y, m, d):
+    return datetime.datetime(year=int(y),month=int(m), day=int(d)).timestamp()
+res = (getSingleEpoch(daymonthyear[0],daymonthyear[1],daymonthyear[2]) - NormalizationMin)/(NormalizationMax -NormalizationMin)
+
+print("The Open Should be at: $" +str(LOBF(res)))
