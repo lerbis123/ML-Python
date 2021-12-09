be76adb701bcb243ec6d7f7a9d54c4094cb5593d
James Sibrel III <jamessibreliii@gmail.com>
2021-12-08 19:59:53 -0500

    Ease of Use and User Error Cleanup
    Added Notes and Cleaned Up code

--- Linear Regression/Stock Guesses.py
+++ Linear Regression/Stock Guesses.py
@@ -12,7 +12,7 @@ print(SampleData.head())
 
 Ticker = ""
 
-#wait for user to pick an accurate ticker
+#Wait for user to pick an accurate ticker
 while Ticker == "":
     print("Please enter a stock name from the following list:")
     i = 0
@@ -30,8 +30,12 @@ while Ticker == "":
         Ticker = inputval
     else:
         print("Please enter a value on the list")
+
+#filter out all data that doesnt have to do with chosen ticker
 filteredData = SampleData[SampleData['ticker'] == Ticker]
 filteredData['date'] = pd.to_datetime(filteredData['date'])
+
+#create epoch column
 def applyepoch(x):
     ts = Timestamp(x)
     return datetime.datetime(year=ts.year,month=ts.month, day=ts.day).timestamp()
@@ -40,7 +44,7 @@ filteredData["epoch"] = epoch
 filteredData = filteredData.sort_values(by=['epoch'],ascending= True)
 print(filteredData.head())
 
-#normalize epoch col
+#Normalization Function
 def Normalize(data: np.array):
     temp1 = np.array(data)
     MyMin,MyMax =  temp1.min(),temp1.max()
@@ -49,21 +53,23 @@ def Normalize(data: np.array):
        retlist = np.append(retlist, (temp1[d] - MyMin)/(MyMax-MyMin))
     return retlist,MyMax,MyMin
 
-#simple linear equation
-# m = slope, c = y intercept 
-##### x*m + c = y
-#apply matrix variables to the equation X = [[series of dates],[series of 1s]], B = [[slope(B0)],[intercept(B1)]],Y = [[series of opening prices]] 
-##### X * B = Y
-#apply the X transpose to the left of both sides
-##### (X^T) * X * B = (X^T) *Y  
-#apply the inverse of the left side to both sides 
-##### B = [(X^T) * X]^-1 (X^T) * Y
-
-#get cols as numpy arrays
+#Get Normalization of epoch col
 NormAndInfo= Normalize(filteredData['epoch'].to_numpy(np.matrix).astype(np.double))
 NormEpochCol = NormAndInfo[0]
 opencol = filteredData['open'].to_numpy(np.matrix).astype(np.double)
+
+#Returns Lambda function of Line of best fit
 def GetLineOfBestFit(Xcol,Ycol):
+    #Linear Equation How I found it
+    # m = slope, c = y intercept 
+    ##### x*m + c = y
+    #apply matrix variables to the equation X = [[series of dates],[series of 1s]], B = [[slope(B0)],[intercept(B1)]],Y = [[series of opening prices]] 
+    ##### X * B = Y
+    #apply the X transpose to the left of both sides
+    ##### (X^T) * X * B = (X^T) *Y  
+    #apply the inverse of the left side to both sides 
+    ##### B = [(X^T) * X]^-1 (X^T) * Y
+    
     #Create X matrix 
     #first col is the date values 
     #second col is filled with 1s for the y intercept
@@ -91,16 +97,35 @@ plt.plot(filteredData["date"].to_numpy(),opencol)
 plt.plot(Xline,Yline)
 plt.show()
 
-#Add Stock Date Guessing
-ret = input("Please Enter A Date To Guess the stock price at (Numeric Format: Year/Month/day)")
+#Stock Cost Date Guessing
+def isdate(string:str):
+        dmy = ret.split("/")
+        isnum = True
+        for num in dmy:
+            isnum=isnum and num.isnumeric()
+        if len(dmy) == 3 and isnum:
+            return True
+        else:
+            return False
+NotFinished = True
+while (NotFinished):
+    ret = input("Please Enter A Date To Guess the stock price at (Numeric Format: Year/Month/day)")
 
-daymonthyear = ret.split("/")
+    if isdate(ret):
+        daymonthyear = ret.split("/")
+        #Need these to normalize user entry to fit existing data
+        NormalizationMax = NormAndInfo[1]
+        NormalizationMin = NormAndInfo[2]
 
-NormalizationMax = NormAndInfo[1]
-NormalizationMin = NormAndInfo[2]
+        #change user entry to epoch and apply normalization
+        def getSingleEpoch(y, m, d):
+            return datetime.datetime(year=int(y),month=int(m), day=int(d)).timestamp()
+        res = (getSingleEpoch(daymonthyear[0],daymonthyear[1],daymonthyear[2]) - NormalizationMin)/(NormalizationMax -NormalizationMin)
 
-def getSingleEpoch(y, m, d):
-    return datetime.datetime(year=int(y),month=int(m), day=int(d)).timestamp()
-res = (getSingleEpoch(daymonthyear[0],daymonthyear[1],daymonthyear[2]) - NormalizationMin)/(NormalizationMax -NormalizationMin)
-
-print("The Open Should be at: $" +str(LOBF(res)))
+        #plug normalized epoch into line of best fit function and print results
+        print("The Open Should be at: $" +str(LOBF(res)))
+    else:
+        print("Entry Invalid")
+    YN=input("Would you like another guess (Y/N)")
+    if(YN=="N"):
+        NotFinished = False
